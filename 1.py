9d217ffaf62a02391a321f10fddca442d8962431
Ravanaki2003shankar <shankarravanaki@gmail.com>
2023-02-16 17:44:34 +0530

    16/2/2023

--- 1.py
+++ 1.py
@@ -14,12 +14,7 @@
 # c=a+b
 # print(c)
 
-# # Store input numbers
-# num1 = input('Enter first number: ')
-# num2 = input('Enter second number: ')
-
-# # Add two numbers
-# sum = float(num1) + float(num2)
-
-# # Display the sum
-# print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# a=3
+# b=6
+# sum=a+b
+# print(sum)
---
+++ s2.py
@@ -0,0 +1,46 @@
+# program to print sum two numbers
+
+# a=3
+# b=6
+# sum=a+b
+# print(sum)
+
+# a,b=22,12
+# print("sum of two numbers",a+b)
+
+#program to find square root of numbers
+# import math
+# print(math.sqrt(25))
+
+# # python program to calculate the area of traingle
+# b,h=12,15
+# area=(0.5*b*h)
+# print("area of triangle",area)
+
+# #python program simple interest
+# p,t,r,=5000,5,6
+# si=p*(t*r)/100
+# print("simple intrest",si)
+
+#python program for compound interest
+# p=1200
+# t=2
+# r=5.4
+# a=p*(1+(r/100))*t
+# ci=a-p
+# print(ci)
+# print("compound interest",ci)
+
+#print("principle amount",1200,"time",2,"rate",5.4,"amount",1200*(1+(5.4/100)**2),"ci=",1329.6-1200)
+
+#program to print area of circle
+# r=10
+# area=3.142*r*r
+# print(" area of circle",area)
+
+#program to find swap of numbers 
+# p,q=132,58
+# p,q=q,p
+# print("value",q)
+
+#python program to check if a numbersis positive negative or 0
