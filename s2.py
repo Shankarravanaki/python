1ae4f56cede212768adc0fcdea7a74e9722062f5
Shankarravanaki <124365522+Shankarravanaki@users.noreply.github.com>
2023-02-16 17:46:24 +0530

    Update s2.py

--- s2.py
+++ s2.py
@@ -1,4 +1,4 @@
-# program to print sum two numbers
+#1. program to print sum two numbers
 
 # a=3
 # b=6
@@ -8,21 +8,21 @@
 # a,b=22,12
 # print("sum of two numbers",a+b)
 
-#program to find square root of numbers
+#2.program to find square root of numbers
 # import math
 # print(math.sqrt(25))
 
-# # python program to calculate the area of traingle
+# #3. python program to calculate the area of traingle
 # b,h=12,15
 # area=(0.5*b*h)
 # print("area of triangle",area)
 
-# #python program simple interest
+# #4.python program simple interest
 # p,t,r,=5000,5,6
 # si=p*(t*r)/100
 # print("simple intrest",si)
 
-#python program for compound interest
+#5.python program for compound interest
 # p=1200
 # t=2
 # r=5.4
@@ -31,14 +31,14 @@
 # print(ci)
 # print("compound interest",ci)
 
-#print("principle amount",1200,"time",2,"rate",5.4,"amount",1200*(1+(5.4/100)**2),"ci=",1329.6-1200)
+#print6.("principle amount",1200,"time",2,"rate",5.4,"amount",1200*(1+(5.4/100)**2),"ci=",1329.6-1200)
 
-#program to print area of circle
+#7.program to print area of circle
 # r=10
 # area=3.142*r*r
 # print(" area of circle",area)
 
-#program to find swap of numbers 
+#8.program to find swap of numbers 
 # p,q=132,58
 # p,q=q,p
 # print("value",q)
