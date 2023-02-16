d58ef78fbcfe63ec0ee5d862a4ce81193de36173
Ravanaki2003shankar <124365522+Ravanaki2003shankar@users.noreply.github.com>
2023-02-16 16:04:04 +0530

    Update 1.py

--- 1.py
+++ 1.py
@@ -23,24 +23,3 @@
 
 # # Display the sum
 # print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-# Python Program to find the area of triangle
-float (a,b,c):
-
-print("enter one side a= :")
-print("enter one side b=: ")
-print("enter one side c= :")
-# a = 5
-# b = 6
-# c = 7
-
-# Uncomment below to take inputs from the user
-# a = float(input('Enter first side: '))
-# b = float(input('Enter second side: '))
-# c = float(input('Enter third side: '))
-
-# calculate the semi-perimeter
-s = (a + b + c) / 2
-
-# calculate the area
-area = (s*(s-a)*(s-b)*(s-c)) ** 0.5
-print('The area of the triangle is %0.2f' %area)
