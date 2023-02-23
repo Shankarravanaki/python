319630abd38e9e68f844b6ca4cb243dc27a5d481
Ravanaki2003shankar <shankarravanaki@gmail.com>
2023-02-23 17:41:32 +0530

    123

---
+++ s3.py
@@ -0,0 +1,89 @@
+# import tkinter as tk
+# r = tk.Tk()
+# r.title('Counting Seconds')
+# button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+# button.pack()
+# r.mainloop()
+
+
+# from tkinter import *
+# master = Tk()
+# w = Canvas(master, width=40, height=60)
+# w.pack()
+# canvas_height=20
+# canvas_width=200
+# y = int(canvas_height / 2)
+# w.create_line(0, y, canvas_width, y )
+# mainloop()
+
+# from tkinter import *
+# master = Tk()
+# var1 = IntVar()
+# Checkbutton(master, text='male', variable=var1).grid(row=0, sticky=W)
+# var2 = IntVar()
+# Checkbutton(master, text='female', variable=var2).grid(row=1, sticky=W)
+# mainloop()
+
+# from tkinter import *
+# master = Tk()
+# Label(master, text='First Name').grid(row=0)
+# Label(master, text='Last Name').grid(row=1)
+# e1 = Entry(master)
+# e2 = Entry(master)
+# e1.grid(row=0, column=1)
+# e2.grid(row=1, column=1)
+# mainloop()
+
+# from tkinter import *
+  
+# root = Tk()
+# frame = Frame(root)
+# frame.pack()
+# bottomframe = Frame(root)
+# bottomframe.pack( side = BOTTOM )
+# redbutton = Button(frame, text = 'Red', fg ='red')
+# redbutton.pack( side = LEFT)
+# greenbutton = Button(frame, text = 'Brown', fg='brown')
+# greenbutton.pack( side = LEFT )
+# bluebutton = Button(frame, text ='Blue', fg ='blue')
+# bluebutton.pack( side = LEFT )
+# blackbutton = Button(bottomframe, text ='Black', fg ='black')
+# blackbutton.pack( side = BOTTOM)
+# root.mainloop()
+
+#1
+# from tkinter import *
+# master = Tk()
+# var1 = IntVar()
+# Checkbutton(master, text='student name', variable=var1).grid(row=0, sticky=W)
+# var2 = IntVar()
+# Checkbutton(master, text='student register number', variable=var2).grid(row=1, sticky=W)
+# mainloop()
+
+
+#2
+# import tkinter as tk
+# r = tk.Tk()
+# r.title('Counting Seconds')
+# button = tk.Button(r, text='start', width=25, command=r.destroy)
+# button.pack()
+# r.mainloop()
+
+#3
+# from tkinter import *
+# master = Tk()
+# Label(master, text='brother  Name').grid(row=0)
+# Label(master, text='sister Name').grid(row=1)
+# e1 = Entry(master)
+# e2 = Entry(master)
+# e1.grid(row=0, column=1)
+# e2.grid(row=1, column=1)
+# mainloop()
+
+
+
+
+
+
+
+
