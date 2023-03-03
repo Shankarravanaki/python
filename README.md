5cf31ccb4f222405ebb8ca764a690bed12199eba
Ravanaki2003shankar <shankarravanaki@gmail.com>
2023-03-03 18:22:41 +0530

    3/3/2023

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 print("kionics INDIA")
 
+
--- s3.py
+++ s3.py
@@ -69,7 +69,7 @@
 # button.pack()
 # r.mainloop()
 
-#3
+# 3
 # from tkinter import *
 # master = Tk()
 # Label(master, text='brother  Name').grid(row=0)
@@ -80,10 +80,53 @@
 # e2.grid(row=1, column=1)
 # mainloop()
 
+#4
 
+# from tkinter import *
+  
+# root = Tk()
+# frame = Frame(root)
+# frame.pack()
+# bottomframe = Frame(root)
+# bottomframe.pack( side = BOTTOM )
+# redbutton = Button(frame, text = 'yellow', fg ='red')
+# redbutton.pack( side = LEFT)
+# greenbutton = Button(frame, text = 'pink', fg='orange')
+# greenbutton.pack( side = LEFT )
+# bluebutton = Button(frame, text ='red', fg ='blue')
+# bluebutton.pack( side = LEFT )
+# blackbutton = Button(bottomframe, text ='green', fg ='brown')
+# blackbutton.pack( side = BOTTOM)
+# root.mainloop()
 
+#5
+         # x=10
+# y,y=x+2,x+3
+# y,y=12,13
+# print(y)
 
+#27/2/2023
+# def fun (a=12,b=12):
+#     print(a//9)
+#     return(a*b)
+# fun(a=9,b=2)
 
+# a=[10,20]
+# b=a
+# b+=[30,40]
+# c=b
+# c+=[50,60]
+# e=c
+# e+=("hello world")
+# print(a)
 
+# n=10
+# for i in range(1,n+1):
+#     print(""*(i-1),end="")
+#     print("*"*(2*(n-i)+1))
 
-
+# def triangle(n):
+#     for i in range(1,n+1):
+#         print(""*(n-i)+"*"*(i))
+    
+    
