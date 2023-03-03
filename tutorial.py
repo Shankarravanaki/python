6246c308d591ff611143f135edc61adb0dfed5ab
Ravanaki2003shankar <shankarravanaki@gmail.com>
2023-03-03 18:59:53 +0530

    12

---
+++ tutorial.py
@@ -0,0 +1,16 @@
+
+
+# list1 = []
+# n = int(input("Enter the n value: "))
+# print("Enter the elements: ")
+# for i in range(n):
+#     list1.append(int(input()))
+# n=len(list1)
+# for i in range(n-1,0,-1):
+#     for j in range(i):
+#         if(list1[j] > list1[j+1]):
+#             temp = list1[j]         # temp = 10
+#             list1[j]=list1[j+1]       # 10=30
+#             list1[j+1]=temp           # 30=temp
+#     print(list1)
+# print(list1)
