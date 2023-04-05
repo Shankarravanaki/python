f01cedd55512ae1faeef101675ba0e8cf2cb9c37
shankarravanaki <shankaravanaki@gmail.comr>
2023-04-05 18:56:03 +0530

    12

---
+++ s3.py
@@ -0,0 +1,134 @@
+# import tkinter as tk
+# r = tk.Tk()
+# r.title('Counting Seconds')
+# button = tk.Button(r, text='Stop', width=25, command=r.destroy)
+# button.pack()
+# r.mainloop()
+
+
+# from tkinter import *
+# master = Tk()
+# w = Canvas(master, width=40, height=60)
+# w.pack()
+# canvas_height=20
+# canvas_width=200
+# y = int(canvas_height / 2)
+# w.create_line(0, y, canvas_width, y )
+# mainloop()
+
+# from tkinter import *
+# master = Tk()
+# var1 = IntVar()
+# Checkbutton(master, text='male', variable=var1).grid(row=0, sticky=W)
+# var2 = IntVar()
+# Checkbutton(master, text='female', variable=var2).grid(row=1, sticky=W)
+# mainloop()
+
+# from tkinter import *
+# master = Tk()
+# Label(master, text='First Name').grid(row=0)
+# Label(master, text='Last Name').grid(row=1)
+# e1 = Entry(master)
+# e2 = Entry(master)
+# e1.grid(row=0, column=1)
+# e2.grid(row=1, column=1)
+# mainloop()
+
+# from tkinter import *
+  
+# root = Tk()
+# frame = Frame(root)
+# frame.pack()
+# bottomframe = Frame(root)
+# bottomframe.pack( side = BOTTOM )
+# redbutton = Button(frame, text = 'Red', fg ='red')
+# redbutton.pack( side = LEFT)
+# greenbutton = Button(frame, text = 'Brown', fg='brown')
+# greenbutton.pack( side = LEFT )
+# bluebutton = Button(frame, text ='Blue', fg ='blue')
+# bluebutton.pack( side = LEFT )
+# blackbutton = Button(bottomframe, text ='Black', fg ='black')
+# blackbutton.pack( side = BOTTOM)
+# root.mainloop()
+
+#1
+# from tkinter import *
+# master = Tk()
+# var1 = IntVar()
+# Checkbutton(master, text='student name', variable=var1).grid(row=0, sticky=W)
+# var2 = IntVar()
+# Checkbutton(master, text='student register number', variable=var2).grid(row=1, sticky=W)
+# mainloop()
+
+
+#2
+# import tkinter as tk
+# r = tk.Tk()
+# r.title('Counting Seconds')
+# button = tk.Button(r, text='start', width=25, command=r.destroy)
+# button.pack()
+# r.mainloop()
+
+# 3
+# from tkinter import *
+# master = Tk()
+# Label(master, text='brother  Name').grid(row=0)
+# Label(master, text='sister Name').grid(row=1)
+# e1 = Entry(master)
+# e2 = Entry(master)
+# e1.grid(row=0, column=1)
+# e2.grid(row=1, column=1)
+# mainloop()
+
+#4
+
+# from tkinter import *
+  
+# root = Tk()
+# frame = Frame(root)
+# frame.pack()
+# bottomframe = Frame(root)
+# bottomframe.pack( side = BOTTOM )
+# redbutton = Button(frame, text = 'yellow', fg ='red')
+# redbutton.pack( side = LEFT)
+# greenbutton = Button(frame, text = 'pink', fg='orange')
+# greenbutton.pack( side = LEFT )
+# bluebutton = Button(frame, text ='red', fg ='blue')
+# bluebutton.pack( side = LEFT )
+# blackbutton = Button(bottomframe, text ='green', fg ='brown')
+# blackbutton.pack( side = BOTTOM)
+# root.mainloop()
+
+#5
+         # x=10
+# y,y=x+2,x+3
+# y,y=12,13
+# print(y)
+
+#27/2/2023
+# def fun (a=12,b=12):
+#     print(a//9)
+#     return(a*b)
+# fun(a=9,b=2)
+
+# a=[10,20]
+# b=a
+# b+=[30,40]
+# c=b
+# c+=[50,60]
+# e=c
+# e+=("hello world")
+# print(a)
+
+# n=10
+# for i in range(1,n+1):
+#     print(""*(i-1),end="")
+#     print("*"*(2*(n-i)+1))
+
+# def triangle(n):
+#     for i in range(1,n+1):
+#         print(""*(n-i)+"*"*(i))
+    
+# s="hello python"
+# # print(s)
+# s[-4]
