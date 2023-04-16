0b90adc76bb9501836a35477c9b8eecc87281847
shankarravanaki <shankaravanaki@gmail.comr>
2023-04-16 17:06:58 +0530

    132

---
+++ python/pythone module1/1.py
@@ -0,0 +1,48 @@
+# # Which is As Follows: EMI = (P X R/12) X [(1+R/12) ^N] / [(1+R/12) ^N-1]. 
+
+
+# import tkinter as tk
+
+# main=tk.Tk(className="emi calculator")
+# main.geometry("400x400")
+
+# def call():
+#     input_value=txt1.get()
+#     print(input)
+
+# # def call():
+# #     input_value=txt2.get()
+# #     print(input)
+
+# name1=tk.Label(main,text="python")
+# txt1=tk.Entry(main)
+# btn1=tk.Button(main,text="submit",command=call)
+
+
+
+# name1.grid(row=0,column=0)
+# txt1.grid(row=0,column=1)
+
+
+
+# password=tk.Label(main,text="password")
+# txt2=tk.Entry(main)
+# btn2=tk.Button(main,text="submit",command=call)
+
+# password.grid(row=3,column=0)
+# txt2.grid(row=3,column=1)
+
+# btn1.grid(row=4,column=1)
+
+# main.mainloop()
+
+
+import tkinter as tk
+
+main=tk.Tk()
+'''hello
+python'''
+
+main.mainloop()
+
+
