a6f117360ac9ec31ffd0002637e00258a48a8da4
shankarravanaki <shankarravanaki>
2023-05-17 17:23:05 +0530

    1

--- python/pythone module1/Tkinter.py
+++ python/pythone module1/Tkinter.py
@@ -32,50 +32,27 @@
 # from tkinter import *
 import tkinter as tk
 
-def call():
-    
-    input_label=(Txt1.get())
-    print(input_label)
-    input_label2=(txt2.get())
-    print(input_label2)
-    input_label3=(txt3.get())
-    print(input_label3)
-    input_label4=(txt4.get())
-    print(input_label4)
-    input_label5=(txt5.get())
-    print(input_label5)
-    input_label6=(txt6.get())
-    print(input_label6)
-
-
-main=tk.Tk(className="student id ")
+main=tk.Tk(className="customer details ")
 main.geometry("400x400")
+main_lst=[]
+
+def add():
+    lst=[Name.get(),age.get(),contact.get()]
+    main_lst.append(lst)
+
+with open["C:/Users/User/Desktop/customer details.csv","w"]
 
 
+label1=tk.Label(main,text="Name").grid(row=0,column=1)
+Txt1=tk.Entry(main).grid(row=0,column=2)
 
-label1=tk.Label(main,text="student Name").grid(row=0,column=0)
-Txt1=tk.Entry(main).grid(row=0,column=1)
+label1=tk.Label(main,text="Age").grid(row=1,column=1)
+Txt1=tk.Entry(main).grid(row=1,column=2)
 
-label2=tk.Label(main,text="STUDENT CLASS").grid(row=1,column=0)
-txt2=tk.Entry(main).grid(row=1,column=1)
+label1=tk.Label(main,text="Contact").grid(row=2,column=1)
+Txt1=tk.Entry(main).grid(row=2,column=2)
 
-label3=tk.Label(main,text="STUDENT ROLL NUMBER").grid(row=2,column=0)
-txt3=tk.Entry(main).grid(row=2,column=1)
-
-labe4=tk.Label(main,text="STUDENT DOB").grid(row=3,column=0)
-txt4=tk.Entry(main).grid(row=3,column=1)
-
-labe5=tk.Label(main,text="STUDENT AREA").grid(row=4,column=0)
-txt5=tk.Entry(main).grid(row=4,column=1)
-
-labe6=tk.Label(main,text="PHONE NUMBER").grid(row=5,column=0)
-txt6=tk.Entry(main).grid(row=5,column=1)
-
-btn1=tk.Button(main,text="SUBMIT",command=call).grid(row=6,column=1)
-
+btn1=tk.Button(main,text="submit").grid(row=3,column=2)
 
 main.mainloop()
-
-
-
 
