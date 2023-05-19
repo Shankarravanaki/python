83f431e57aee2cd3e7df8d04be7b055e2510f064
Shankarravanaki <Shankarravanaki@gmail.com>
2023-05-19 11:46:11 +0530

    1

--- python/pythone module1/Tkinter.py
+++ python/pythone module1/Tkinter.py
@@ -29,18 +29,19 @@
 # student_id=tk.Label(main,text="student id").grid(row=8,column=1)
 
 
-# from tkinter import *
+import csv
 import tkinter as tk
+from tkinter import messagebox
+
+def call():
+    input_value=Txt1.__geta
+
 
 main=tk.Tk(className="customer details ")
 main.geometry("400x400")
 main_lst=[]
 
-def add():
-    lst=[Name.get(),age.get(),contact.get()]
-    main_lst.append(lst)
 
-with open["C:/Users/User/Desktop/customer details.csv","w"]
 
 
 label1=tk.Label(main,text="Name").grid(row=0,column=1)
@@ -52,7 +53,7 @@ Txt1=tk.Entry(main).grid(row=1,column=2)
 label1=tk.Label(main,text="Contact").grid(row=2,column=1)
 Txt1=tk.Entry(main).grid(row=2,column=2)
 
-btn1=tk.Button(main,text="submit").grid(row=3,column=2)
+btn1=tk.Button(main,text="submit",command=call).grid(row=3,column=2)
 
 main.mainloop()
 
